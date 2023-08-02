cdfbc82553b48d7dfce01c071b916c429a2854f9
Greymassive <padiktolya@gmail.com>
2023-08-02 13:18:16 +0300

    jacoco set

--- src/test/java/ru/netology/statistic/StatisticsServiceTest.java
+++ src/test/java/ru/netology/statistic/StatisticsServiceTest.java
@@ -17,4 +17,18 @@ public class StatisticsServiceTest {
 
         Assertions.assertEquals(expected, actual);
     }
+
+    @Test
+    void findMaxIfLast() {
+        StatisticsService service = new StatisticsService();
+
+        long[] incomesInBillions = {10, 5, 8, 4, 5, 3, 8, 6, 11, 11, 13};
+        long expected = 13;
+
+        long actual = service.findMax(incomesInBillions);
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+
 }
